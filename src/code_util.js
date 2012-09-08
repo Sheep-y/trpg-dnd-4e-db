86e10a1ac5ff25b5ca40d14ded72b08656a9c5b9
Anonymous <anonymous@example.net>
2012-09-09 07:52:56 +0800

    Browser detection, noscript, and basic dom query

--- src/code_util.js
+++ src/code_util.js
@@ -1,6 +1,22 @@
 <script>'use strict';
 /*
  * code_util.js
- * Utility codes
+ * Utility codes, including initial detection of browser feature and no script.
  */
-</script>
+
+// Main namespace
+var oddi = {};
+ 
+// Simple check for browser features
+if ( ! document.querySelectorAll || !window.Storage ) {
+   alert('Please upgrade browser.');
+}
+ 
+function _( cssSelector ) {
+   return document.querySelectorAll( cssSelector );
+}
+
+</script><noscript>
+   <h1> Please enable JavaScript </h1>
+   <h1> 請啓用 JavaScript </h1>
+</noscript>
