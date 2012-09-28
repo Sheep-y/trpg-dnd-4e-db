115c393351fe1a1ddc7985c5ea9f3a8ebfb4f760
Anonymous <anonymous@example.net>
2012-09-28 23:00:06 +0800

    Switch to Ant build

--- compile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd src
-copy html_doctype.html+style.css+html_header.html+code_util.js+code_gui.js+data_model.js+data_read.js+data_index.js+data_write.js+action_download.html+action_list.html+action_view.html+html_footer.html offline_ddi.html
-cd ..
-del offline_ddi.html
-move src\offline_ddi.html .
