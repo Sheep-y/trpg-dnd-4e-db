90d4da3881d86fa73c662b9f7dff07fe096507c4
Anonymous <anonymous@example.net>
2012-10-05 13:05:15 +0800

    Add list page and navigation from and to it.

--- src/lang_en.js
+++ src/lang_en.js
@@ -12,20 +12,26 @@ _.l.set( 'error', {
 });
 
 _.l.set( 'action.download', {
-   title: "Offline Compendium",
-   paragraph: [
+   title : "Update Offline Compendium",
+   paragraph : [
       "This browser script allows you to download and save compendium entries, then search and list them offline using advanced Google-like syntax.",
       "<b>Requires an active DDI subscription to download any data.</b> <a href='http://www.wizards.com/DnD/Article.aspx?x=dnd/updates' target='_blank'>Official Rules Updates</a>"
    ],
-   btn_refresh: "Refresh list",
-   chk_refresh: "Auto refresh",
-   btn_update_changed: "Update %1 new and %2 changed",
-   btn_update_all: "Update all %1",
-   btn_clear_existing: "Clear Data",
-   lbl_new_items: "New Entries",
-   lbl_changed_items: "Changed Entries",
-   lbl_deleted_items: "Removed Entries",
-   msg_clearExisting: "Delete existing data?",
+   btn_refresh : "Refresh list",
+   chk_refresh : "Auto refresh",
+   btn_clear_existing : "Clear Data",
+   btn_update_changed : "Update %1 new and %2 changed",
+   btn_update_all : "Update all %1",
+   btn_done       : "Browse Data",
+   lbl_new_items  : "New Entries",
+   lbl_changed_items : "Changed Entries",
+   lbl_deleted_items : "Removed Entries",
+   msg_clearExisting : "Delete existing data?",
+});
+
+_.l.set( 'action.list', {
+   title : "Serch Offline Compendium",
+   btn_update : "Update Compendium",
 });
 
 </script>
