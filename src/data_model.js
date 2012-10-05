3e23e741558839d4f8c3991baf0ef36c836df5f8
Anonymous <anonymous@example.net>
2012-10-05 11:57:49 +0800

    Add clear data button

--- src/data_model.js
+++ src/data_model.js
@@ -11,7 +11,7 @@ oddi.data = {
          "columns": [ "ID", "Name", "Category", "SourceBook" ],
          "listing": [ [ "sampleId001", "Sample Data", "Sample1", "Git" ],
                     [ "sampleId003", "Sample Data 3", "Sample3", "Git" ], ],
-         "index": {"this":[0,1],"sample":[0],"data":[0,1],"sampleId001":[0],"another":[1],"that":[1],"example":[1]},
+         //"index": {"this":[0,1],"sample":[0],"data":[0,1],"sampleId001":[0],"another":[1],"that":[1],"example":[1]},
          "data": [ "<p>This is sample data id sampleId001</p>" ], // Data at index 2 not loaded yet
       }
    },
@@ -86,12 +86,12 @@ oddi.data = {
 
    parse : function data_parse( data ) {
       // Extract body
-      data = data.trim().match( /<body[^>]*>((?:.|\n)*)<\/body\s*>/ );
+      data = data.match( /<body[^>]*>((?:.|\n)*)<\/body\s*>/ );
       if ( !data ) return null;
       // Normalise whitespace
-      data = data[1].replace( /\n+/g, ' ' ).replace( /\s+/g, ' ' );
+      data = data[1].trim().replace( /\n+/g, ' ' ).replace( /\s+/g, ' ' );
       // Remove script and form tags
-      data = data.replace( /<script\b.*?<\/script\s*>/g, '' ).replace( /<input[^>]*>/g, '').replace( /<form[^>]*>|<\/form\s*>/g, '' );
+      data = data.trim().replace( /<script\b.*?<\/script\s*>/g, '' ).replace( /<input[^>]*>/g, '').replace( /<form[^>]*>|<\/form\s*>/g, '' );
       // Remove empty contents
       data = data.replace( /<div[^>]*>\s*<\/div\s*>/g, '' );
       return {
