0150c9e166ade39c6808dd95e4f64750cd51820f
Anonymous <anonymous@example.net>
2012-10-21 19:53:58 +0800

    Refactor and bug fix data writer

--- src/data_writer.js
+++ src/data_writer.js
@@ -8,104 +8,98 @@
 oddi.writer = {
    _fs : null,
 
-   _write : function writer_write( file, content ) {
+   _call : function writer_call ( com_callback ) {
       if ( window.ActiveXObject ) {
-         // IE ActiveX writer. Write file in UTF-16 and windows linebreak, not much choice here.
-         //file = oddi.config.data_full_path + file;
-         try {
-            var fso = oddi.writer._fs;
-            if ( oddi.writer._fs === null ) {
-               alert( _.l( 'error.file_grant_permission' ) );
-               fso = oddi.writer._fs = new ActiveXObject("Scripting.FileSystemObject");
-            }
-            if ( ! fso.FolderExists( oddi.config.data_full_path ) ) fso.CreateFolder( oddi.config.data_full_path );
-            folder = file.replace( /[\/\\][^\/\\:*?"<>|]+$/, '' );
-            if ( ! fso.FolderExists( folder ) ) {
-               fso.CreateFolder( folder );
-            }
-            /*// No need to delete, file will be overwritten
-            else if ( fso.FileExists( file ) ) {
-               _.log( 'Delete '+file );
-               fso.DeleteFile( file );
-               if ( fso.FileExists( file ) ) {
-                  _.error( _.l( 'error.file_cannot_delete', file ) );
-                  return false;
+         if ( com_callback ) {
+            try {
+               var fso = oddi.writer._fs;
+               if ( fso === null ) {
+                  alert( _.l( 'error.file_grant_permission' ) );
+                  fso = oddi.writer._fs = new ActiveXObject("Scripting.FileSystemObject");
                }
-            }*/
-            _.log( 'Write to '+file );
-            var file = fso.OpenTextFile( file, 2, true, -1); // = OpenTextFile( file, WRITE, CREATE, UNICODE )
-            content.split( '\n' ).forEach( function(line){ file.WriteLine( line ); } );
-            file.Close();
-            _.log( 'Done' );
-         } catch (e) {
-            if (e.number == -2146827859) alert('Unable to access local files due to browser security settings. Go to Tools->Internet Options->Security->Custom Level. Enable "Initialize and script ActiveX controls not marked as safe".');
-            else alert(e);
+               com_callback( fso );
+            } catch (e) {
+               if (e.number == -2146827859) alert( _.l( 'error.com_file_security') );
+               else _.error(e);
+            }
          }
       } else {
          alert( _.l( 'error.file_no_api' ) );
-         return false;
       }
    },
 
-   _delete : function writer_write( file ) {
-      if ( window.ActiveXObject ) {
+   _write : function writer_write( file, content, ondone ) {
+      // IE ActiveX writer. Write file in UTF-16 and windows linebreak, not much choice here.
+      //file = oddi.config.data_full_path + '/' + file;
+      this._call( function writer_com_write( fso ) {
+         _.log( 'Write to '+file );
+         // Create root data folder if not exists
+         if ( ! fso.FolderExists( oddi.config.data_full_path ) ) {
+            fso.CreateFolder( oddi.config.data_full_path );
+         }
+         // Create containing folder if not exists
+         var folder = file.replace( /[\/\\][^\/\\:*?"<>|]+$/, '' );
+         if ( ! fso.FolderExists( folder ) ) {
+            fso.CreateFolder( folder );
+         }
+         /*// No need to delete, file will be overwritten
+         else if ( fso.FileExists( file ) ) {
+            _.log( 'Delete '+file );
+            fso.DeleteFile( file );
+            if ( fso.FileExists( file ) ) {
+               _.error( _.l( 'error.file_cannot_delete', file ) );
+               return false;
+            }
+         }*/
+         var f = fso.OpenTextFile( file, 2, true, -1); // = OpenTextFile( file, WRITE, CREATE, UNICODE )
+         content.split( '\n' ).forEach( function(line){ f.WriteLine( line ); } );
+         f.Close();
+         if ( ondone ) ondone();
+      }, null );
+   },
+
+   _delete : function writer_write( file, ondone ) {
+      this._call( function writer_com_delete( fso ) {
          // IE ActiveX file system.
          file = oddi.writer.basePath + file;
-         try {
-            var fso = oddi.writer._fs;
-            if ( oddi.writer._fs === null ) {
-               alert( _.l( 'error.file_grant_permission' ) );
-               fso = oddi.writer._fs = new ActiveXObject("Scripting.FileSystemObject");
-            }
+         if ( fso.FileExists( file ) ) {
+            _.log( 'Delete '+file );
+            fso.DeleteFile( file );
             if ( fso.FileExists( file ) ) {
-               _.log( 'Delete '+file );
-               fso.DeleteFile( file );
-               if ( fso.FileExists( file ) ) {
-                  _.error( _.l( 'error.file_cannot_delete', file ) );
-                  return false;
-               }
+               return _.error( _.l( 'error.file_cannot_delete', file ) );
             }
-         } catch (e) {
-            if (e.number == -2146827859) alert( _.l('error.com_file_security'));
-            else alert(e);
          }
-      } else {
-         alert( _.l( 'error.file_no_api' ) );
-         return false;
-      }
+         if ( ondone ) ondone();
+      }, null );
    },
 
    // Writing is instantaneous, but let's use callback for future compatibility
-   write_index : function writer_write_category( onload ) {
+   write_index : function writer_write_category( ondone ) {
      // oddi.reader.jsonp_index( 20120915, {
       var object = {}, data = oddi.data.category;
       for ( var cat in data ) object[cat] = data[cat].count();
       this._write( oddi.file.index(),
-         'oddi.reader.jsonp_index( 20120915,' + JSON.stringify( object ) + ')' );
-      if ( onload ) onload();
+         'oddi.reader.jsonp_index(20120915,' + JSON.stringify( object ) + ')', ondone );
    },
 
-   write_data_listing : function writer_write_data_listing( category, onload ) {
+   write_data_listing : function writer_write_data_listing( category, ondone ) {
      // oddi.reader.jsonp_data_listing( 20120915, "Sample", [ "Id", "Name", "Category", "SourceBook" ], [
       this._write( oddi.file.category_listing( category.name ),
-         'oddi.reader.jsonp_data_listing( 20120915, "'+_.escJs(category.name)+'",'
+         'oddi.reader.jsonp_data_listing(20120915,"'+_.escJs(category.name)+'",'
          +JSON.stringify( category.columns ) + ','
-         +JSON.stringify( category.listing ) + ')' );
-      if ( onload ) onload();
+         +JSON.stringify( category.listing ) + ')', ondone );
    },
 
-   write_data_index : function writer_write_data_index( category, onload ) {
+   write_data_index : function writer_write_data_index( category, ondone ) {
       this._write( oddi.file.category_index( category.name ),
-       'oddi.reader.jsonp_data_index( 20121020, "'+_.escJs(category.name)+'",'
-       +JSON.stringify( category.index ) + ')' );
-      if ( onload ) onload();
+       'oddi.reader.jsonp_data_index(20121020,"'+_.escJs(category.name)+'",'
+       +JSON.stringify( category.index ) + ')', ondone );
    },
 
-   write_data : function writer_write_data( category, from, to, onload ) {
-      this._write( oddi.file.category_index( category.name ),
-       'oddi.reader.jsonp_data(20120915, "'+_.escJs(category.name)+'",'+from+','+
-       JSON.stringify( category.data.slice( from, to ) ) + ')' );
-      if ( onload ) onload();
+   write_data : function writer_write_data( category, from, to, ondone ) {
+      this._write( oddi.file.data( category.name, from ),
+       'oddi.reader.jsonp_data(20120915,"'+_.escJs(category.name)+'",'+from+','+
+       JSON.stringify( category.data.slice( from, to ) ) + ')', ondone );
    },
 
 };
