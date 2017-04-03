59c234a7e0a90886cffc6f8b08227b8941d5355c
Sheep-y <your.sheepy@gmail.com>
2017-04-03 13:04:38 +0800

    Split creature type into CreatureConverter.

--- java/db4e/converter/MonsterConverter.java
+++ java/db4e/converter/MonsterConverter.java
@@ -1,37 +1,21 @@
 package db4e.converter;
 
 import db4e.data.Category;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import sheepy.util.Utils;
 
-public class MonsterConverter extends LeveledConverter {
-
-   private static final int SIZE = 3;
-   private static final int TYPE = 4;
-
-   private final Set<String> AllSizes = new HashSet<>( Arrays.asList( new String[]{
-      "tiny", "small", "medium", "large", "huge", "gargantuan" } ) );
-   private final Set<String> KeywordBlackList = new HashSet<>( Arrays.asList( new String[]{
-      "female", "or" } ) ); // Female: monster115831 (x3), Or: monster1466 (x1)
-
-   private final Set<String> keywords = new TreeSet<>();
-   private final Set<String> sizes = new TreeSet<>( MonsterConverter::sizeSort );
+public class MonsterConverter extends CreatureConverter {
 
    public MonsterConverter(Category category) {
-      super(category);
+      super( category );
+      SIZE = 3;
+      TYPE = 4;
+      KeywordBlackList.add( "female" ); // monster115831 (x3)
+      KeywordBlackList.add( "or" ); // monster1466 (x1)
    }
 
    @Override protected void initialise () {
       category.fields = new String[]{ "Level", "CombatRole", "GroupRole", "Size", "CreatureType", "SourceBook" };
       super.initialise();
    }
-
-   private final Matcher regxType = Pattern.compile( "<span class=type>(.*?)(,\\s*[^<()]+)?</span>" ).matcher( "" );
 
    @Override protected void convertEntry () {
       Object[] fields = entry.getFields();
@@ -63,55 +47,6 @@ public class MonsterConverter extends LeveledConverter {
                fix( "fix basic attack" );
             }
       }
-      if ( find( regxType ) ) {
-         do {
-            String termText = regxType.group( 1 ).trim().toLowerCase().replaceAll( "[/,()]+", " " );
-            if ( ! termText.isEmpty() ) // type and keywords
-               keywords.addAll( Arrays.asList( termText.split( "\\s+" ) ) );
-            String race = regxType.group( 2 ); // race, e.g. drow, human,
-            if ( race != null ) {
-               race = race.substring( 1 ).trim();
-               if ( ! race.trim().contains( " " ) ) // Exclude rare races like "red dragon", "fang titan drake", "dark one", "mind flayer" etc.
-                  keywords.add( race );
-            }
-         } while ( regxType.find() );
-         sizes.addAll( keywords );
-         sizes.retainAll( AllSizes );
-         keywords.removeAll( AllSizes );
-         keywords.removeAll( KeywordBlackList );
-         if ( sizes.isEmpty() ) {
-            sizes.add( "medium" );
-            swap( "<span class=type>", "<span class=type>Medium " );
-            fix( "missing content" );
-         }
-         meta( SIZE, String.join( ", ", sizes.stream().map( Utils::ucfirst ).toArray( String[]::new ) ) );
-         meta( TYPE, String.join( ", ", keywords.stream().map( Utils::ucfirst ).toArray( String[]::new ) ) );
-         sizes.clear();
-         keywords.clear();
-      } else
-         warn( "Creature type not found" );
-   }
-
-   private static final int sizeIndex ( String size ) {
-      switch ( size.toLowerCase() ) {
-         case "tiny":
-            return 1;
-         case "small":
-            return 2;
-         case "medium":
-            return 3;
-         case "large":
-            return 4;
-         case "huge":
-            return 5;
-         case "gargantuan":
-            return 6;
-         default:
-            return 0;
-      }
-   }
-
-   private static final int sizeSort ( String a, String b ) {
-      return sizeIndex( a ) - sizeIndex( b );
+      super.correctEntry();
    }
 }
