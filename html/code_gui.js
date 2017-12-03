e0b3117193496ed4115cb2d1167b5fb9e109d215
Sheep-y <your.sheepy@gmail.com>
2017-12-03 22:21:42 +0800

    Fix js minifiy error. Fix css url correction error.

--- html/code_gui.js
+++ html/code_gui.js
@@ -44,8 +44,8 @@ od.gui = {
       _( 'link[rel="icon"]' )[0].href = od.config.data_read_path + "/res/icon.png";
       try { // Fix category icon location for non-default html name.
          var rule = document.styleSheets[1].cssRules[0];
-         if ( rule.selectorText === '.rpg' && rule.styles.backgroundImage )
-            rule.styles.backgroundImage = 'url("' + od.config.data_read_path + '/res/viewer_category_icon.png")';
+         if ( rule && rule.selectorText === '.rpg' && rule.style.backgroundImage )
+            rule.style.backgroundImage = 'url("' + od.config.data_read_path + '/res/viewer_category_icon.png")';
          else
             _.warn( '[GUI] Cannot find rpg css rule to fix path.' );
       } catch ( err ) { _.error( err ); }
