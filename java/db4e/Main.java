f071e8aa9a6ada1cb24bd88bde39bf218e8515db
Sheep-y <your.sheepy@gmail.com>
2017-12-15 00:55:26 +0800

    Fix development mode detection. Set release date.

--- java/db4e/Main.java
+++ java/db4e/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
    static String TITLE = "Compendium downloader";
    static String VERSION = "3.6 (development)";
-   static String UPDATE_TIME = "2017-02-03"; // Any release beyond this time is an update
+   static String UPDATE_TIME = "2017-12-16"; // Any release beyond this time is an update
 
    // Global log ang preference
    public static final Logger log = Logger.getLogger( Main.class.getName() );
@@ -44,7 +44,7 @@ public class Main {
 
    // Main method. No need to check java version because min version is compile target.
    public static void main( String[] args ) {
-      if ( simulate.get() && ! TITLE.contains( "(development)" ) )
+      if ( simulate.get() && ! VERSION.contains( "(development)" ) )
          simulate.set( false );
       log.setLevel( Level.CONFIG );
       try {
