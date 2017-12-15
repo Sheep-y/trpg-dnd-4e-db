47c4b4729be4ca64f6347393c0b5a99dd0595b56
Sheep-y <your.sheepy@gmail.com>
2017-12-16 00:02:51 +0800

    Start of v4 prototyping branch

---
+++ html/v4/res/script.js
@@ -0,0 +1,51 @@
+(function top_init( global ) { "use strict";
+
+const ns = global.od = {};
+
+function isStr ( obj ) { return typeof obj === "string"; }
+function isObj ( obj ) { return typeof obj === "object"; }
+
+function find ( css ) {
+   return document.querySelector( css );
+}
+
+function findAll ( css ) {
+   return document.querySelectorAll( css );
+}
+
+function iter ( obj ) {
+   return isObj( obj ) && Symbol.iterator in obj ? obj : [ obj ];
+}
+
+function iterElem ( obj ) {
+   return isStr( obj ) ? findAll( obj ) : iter( obj );
+}
+
+
+function addEvent ( element, event, handler, options ) {
+   for ( const type of iter( event ) )
+      for ( const elem of iterElem( element ) )
+         for ( const func of iter( handler ) )
+            elem.addEventListener( type, func, options );
+}
+
+
+document.body.classList.remove( "noscript" );
+
+
+(function nav_init( global ) {  const nav = find( "nav" );
+
+   addEvent( "#btn_menu", "click", () => {
+      nav.className = "opened";
+      nav.setAttribute( "aria-hidden", "false" );
+   });
+
+   addEvent( "#menu_close", "click", () => {
+      nav.className = "closed";
+      nav.setAttribute( "aria-hidden", "true" );
+   });
+   
+})();
+
+
+})( window )
