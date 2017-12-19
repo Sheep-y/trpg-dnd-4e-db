d4b60e2ca153b5120370343e41ebd2bc88ac1f27
Sheep-y <your.sheepy@gmail.com>
2017-12-19 23:26:09 +0800

    Index deity domains as lookup for the deity.

--- java/db4e/converter/DeityConverter.java
+++ java/db4e/converter/DeityConverter.java
@@ -2,11 +2,14 @@ package db4e.converter;
 
 import db4e.data.Category;
 import db4e.data.Entry;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DeityConverter extends Converter {
+
+   protected int DOMAINS = 0;
 
    public DeityConverter ( Category category ) {
       super( category );
@@ -23,7 +26,7 @@ public class DeityConverter extends Converter {
       meta( "", meta( 0 ), meta( 1 ) );
       super.convertEntry();
       if ( find( regxDomain ) )
-         meta( 0, regxDomain.group( 1 ) );
+         meta( DOMAINS, regxDomain.group( 1 ) );
    }
 
    @Override protected int sortEntity ( Entry a, Entry b ) {
@@ -34,6 +37,8 @@ public class DeityConverter extends Converter {
    @Override protected Set<String> getLookupName( Entry entry, Set<String> list ) {
       if ( entry.getName().startsWith( "The " ) )
          list.add( entry.getName().substring( 4 ) );
+      if ( ! meta( DOMAINS ).isEmpty() )
+         list.addAll( Arrays.asList( meta( DOMAINS ).split( ", " ) ) );
       return super.getLookupName( entry, list );
    }
 }
