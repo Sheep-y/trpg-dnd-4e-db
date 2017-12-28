9aaa4c2455dee13bd4052f89847de8c4166f2590
Sheep-y <your.sheepy@gmail.com>
2017-12-28 10:26:49 +0800

    Add new Base85 encode/decode methods & simplify static code.

--- java/sheepy/util/Ascii85.java
+++ java/sheepy/util/Ascii85.java
@@ -1,7 +1,5 @@
 /*
  * libxjava - utility library for cross-Java-platform development
- *            ${project.name}
- *
  * Copyright (c) 2010-2011 Marcel Patzlaff (marcel.patzlaff@gmail.com)
  */
 package sheepy.util;
@@ -14,102 +12,107 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
 import java.io.Writer;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import java.nio.charset.Charset;
+import static java.nio.charset.StandardCharsets.US_ASCII;
 
 /* Base85 (RFC 1924)  encode / decoder */
 public class Ascii85 {
-    private static final char[] ENCODABET;
-    private static final char[] DECODABET;
+   private static final char[] ENCODABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~".toCharArray();
+   private static final char[] DECODABET = new char[256];
 
-    private final static long[] FACTORS= {
-              1,
-             85,
-           7225,   // 85^2
-         614125,   // 85^3
-       52200625    // 85^4
-    };
+   private static final long[] FACTORS= {
+      1,
+      85,
+      7225,     // 85^2
+      614125,   // 85^3
+      52200625  // 85^4
+   };
 
-    static {
-        ENCODABET= new char[] {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', ';', '<', '=',
-            '>', '?', '@', '^', '_', '`', '{', '|', '}', '~'
-        };
-        DECODABET= new char[256];
-        for( int i= 0; i < ENCODABET.length; ++i )
-            DECODABET[ ENCODABET[i] ] = (char) i;
-    }
+   static {
+      for( int i= 0; i < ENCODABET.length; ++i )
+         DECODABET[ ENCODABET[i] ] = (char) i;
+   }
 
+   static public int calcDecodedLength ( int encoded_length ) {
+      return (int) Math.ceil( encoded_length * 0.8 );
+   }
 
-    static public byte[] decode( String data )  {
-       try ( ByteArrayOutputStream out = new ByteArrayOutputStream( (int) Math.ceil( data.length() * 0.8 ) ) ) {
-          decode( new ByteArrayInputStream( data.getBytes( UTF_8 ) ), out );
-          return out.toByteArray();
-       } catch ( IOException ex ) {
-          throw new RuntimeException( "Cannot decode from Base85", ex );
-       }
-    }
+   static public int calcEncodedLength ( int decoded_length ) {
+      return (int) Math.ceil( decoded_length * 1.2 ) + 5;
+   }
 
-    static public void decode( InputStream in, OutputStream out ) throws IOException {
-        int b, factor= 5;
-        long sum= 0;
-        while ( ( b = in.read() ) >= 0) {
-            sum += DECODABET[b] * FACTORS[--factor];
-            if(factor == 0) {
-                for(int e= 24; e >= 0; e-= 8)
-                    out.write((int)((sum >>> e) & 0xFF));
-                sum = 0;
-                factor = 5;
+   static public byte[] decode ( String data )  {
+      try ( ByteArrayOutputStream out = new ByteArrayOutputStream( calcDecodedLength( data.length() ) ) ) {
+         decode( new ByteArrayInputStream( data.getBytes( US_ASCII ) ), out );
+         return out.toByteArray();
+      } catch ( IOException ex ) {
+         throw new RuntimeException( "Cannot decode from Base85", ex );
+      }
+   }
+
+   static public String decode ( String data, Charset charset )  {
+      return new String( decode( data ), charset );
+   }
+
+   static public void decode ( InputStream in, OutputStream out ) throws IOException {
+      int b, factor= 5;
+      long sum= 0;
+      while ( ( b = in.read() ) >= 0) {
+         sum += DECODABET[b] * FACTORS[ --factor ];
+         if ( factor == 0) {
+            for(int e= 24; e >= 0; e-= 8)
+               out.write((int)((sum >>> e) & 0xFF));
+            sum = 0;
+            factor = 5;
+         }
+      }
+      // process rest (if present)
+      if ( factor < 5 ) {
+         sum /= FACTORS[factor];
+         for( int e = (3 - factor) * 8; e >= 0; e -= 8)
+            out.write((int)((sum >>> e) & 0xFF));
+      }
+   }
+
+   static public String encode ( byte[] data ) {
+      return encode( data, 0, data.length );
+   }
+
+   static public String encode ( byte[] data, int start, int length ) {
+      StringWriter out = new StringWriter( calcEncodedLength( length ) );
+      try {
+         encode( new ByteArrayInputStream( data, start, length ), out );
+      } catch ( IOException ex ) {
+         throw new RuntimeException( "Cannot encode to Base85", ex );
+      }
+      return out.toString();
+   }
+
+   static public void encode ( InputStream in, OutputStream out ) throws IOException {
+      encode( in, new OutputStreamWriter( out ) );
+   }
+
+   static public void encode ( InputStream in, Writer out ) throws IOException {
+      int b, bytes = 0;
+      long sum = 0;
+      while( ( b = in.read() ) >= 0) {
+         sum = (sum << 8) | b;
+         if ( ++bytes == 4 ) {
+            for ( int e = 4 ; e >= 0 ; e-- ) {
+               out.write( ENCODABET[ (int) ( sum / FACTORS[e] ) ] );
+               sum %= FACTORS[e];
             }
-        }
-        // process rest (if present)
-        if(factor < 5) {
-            sum /= FACTORS[factor];
-            for( int e = (3 - factor) * 8; e >= 0; e -= 8)
-                out.write((int)((sum >>> e) & 0xFF));
-        }
-    }
+            sum = 0;
+            bytes = 0;
+         }
+      }
 
-    static public String encode( byte[] data ) {
-       StringWriter out = new StringWriter( (int) Math.ceil( data.length * 1.2 ) + 5 );
-       try {
-          encode( new ByteArrayInputStream( data ), out );
-       } catch ( IOException ex ) {
-          throw new RuntimeException( "Cannot encode to Base85", ex );
-       }
-       return out.toString();
-    }
-
-    static public void encode( InputStream in, OutputStream out ) throws IOException {
-       encode( in, new OutputStreamWriter( out ) );
-    }
-
-    static public void encode( InputStream in, Writer out ) throws IOException {
-        int b, bytes = 0;
-        long sum = 0;
-        while( ( b = in.read() ) >= 0) {
-            sum = (sum << 8) | b;
-            bytes++;
-            if ( bytes == 4 ) {
-                for ( int e = 4 ; e >= 0 ; e-- ) {
-                    out.write( ENCODABET[ (int) ( sum / FACTORS[e] ) ] );
-                    sum %= FACTORS[e];
-                }
-                sum = 0;
-                bytes = 0;
-            }
-        }
-
-        // padding (if present)
-        if ( bytes > 0 ) {
-            for ( int e = bytes ; e >= 0 ; e-- ) {
-                out.write( ENCODABET[ (int) (sum / FACTORS[e]) ] );
-                sum %= FACTORS[e];
-            }
-        }
-    }
+      // padding (if present)
+      if ( bytes > 0 ) {
+         for ( int e = bytes ; e >= 0 ; e-- ) {
+            out.write( ENCODABET[ (int) (sum / FACTORS[e]) ] );
+            sum %= FACTORS[e];
+         }
+      }
+   }
 }
